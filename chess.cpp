c0e8a18d7f350388d441960fd04327ee76118255
Luqman Arifin <l.arifin.siswanto@gmail.com>
2016-06-16 03:08:06 +0700

    socket working & add .gitignore

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,8 @@
 #include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -6,25 +10,26 @@
 
 using namespace std;
 
-char buffer[205];
+const int BUFFER_SIZE = 256;
+const int XINUC_PORT = 7387;
+const string XINUC_ADDRESS = "xinuc.org";
 
-int sockfd, portno, len;
+void error(string str) {
+  cerr << str << endl;
+  exit(0);
+}
+
+char buffer[BUFFER_SIZE];
+int sockfd, len;
 struct sockaddr_in serv_addr;
 struct hostent *server;
 
-int main() {
-
-  char buffer[256];
-  if (argc < 3) {
-    fprintf(stderr, "usage %s hostname port\n", argv[0]);
-    exit(0);
-  }
-  portno = atoi(argv[2]);
+void initialize_socket() {
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     error("ERROR opening socket");
   }
-  server = gethostbyname(argv[1]);
+  server = gethostbyname(XINUC_ADDRESS.c_str());
   if (server == NULL) {
     fprintf(stderr, "ERROR, no such host\n");
     exit(0);
@@ -34,19 +39,19 @@ int main() {
   bcopy((char *) server->h_addr, 
        (char *) &serv_addr.sin_addr.s_addr,
        server->h_length);
-  serv_addr.sin_port = htons(portno);
+  serv_addr.sin_port = htons(XINUC_PORT);
   if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
     error("ERROR connecting");
   }
-  
-  memset(buffer, 0, sizeof(buffer));
+}
+
+int main() {
+  initialize_socket();
+  bzero(buffer, BUFFER_SIZE);
   while(1) {
-    len = read(sockfd, buffer, 255);
-    if (len < 0) {
-      error("ERROR reading from socket");
-    }
-    printf("%s\n", buffer);
-    memset(buffer, 0, sizeof(buffer));
+    int len = read(sockfd, buffer, BUFFER_SIZE);
+    printf("%d: %s", strlen(buffer), buffer);
+    bzero(buffer, BUFFER_SIZE);
   }
   close(sockfd);
   return 0;
